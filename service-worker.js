d4dd877e4657749d229cda84349eb4e1bf2a7295
xup61069 <xup61069@gmail.com>
2026-08-16 14:56:12 +0800

    Make suggestion activation explicit

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 const MANIFEST_URL = "dist/web/asset-manifest.json";
 const CACHE_PREFIX = "ae-effects-db-";
-const BUILD_VERSION = "b902e806c75c7d17";
+const BUILD_VERSION = "8b6fbdfc0050ef84";
 
 async function installVersion() {
   const manifestResponse = await fetch(MANIFEST_URL, {cache:"no-store"});
