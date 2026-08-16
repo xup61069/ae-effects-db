6e9d8ba1b75f571af3211dddc9bddde01fb60dc4
Kadid <xup61069@gmail.com>
2026-08-16 16:33:09 +0800

    fix: complete mobile touch targets (#125)

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 const MANIFEST_URL = "dist/web/asset-manifest.json";
 const CACHE_PREFIX = "ae-effects-db-";
-const BUILD_VERSION = "f099feb82e389848";
+const BUILD_VERSION = "12dd59037a28fdb7";
 const currentCacheName = () => `${CACHE_PREFIX}${BUILD_VERSION}`;
 
 async function installVersion() {
