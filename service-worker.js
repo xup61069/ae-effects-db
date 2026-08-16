7b9659e54bdc7ea5b4376bfc8a02883f35fa7a9d
xup61069 <xup61069@gmail.com>
2026-08-17 04:26:10 +0800

    web: make site title, browse-all header and kind summary chips clickable

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 const MANIFEST_URL = "dist/web/asset-manifest.json";
 const CACHE_PREFIX = "ae-effects-db-";
-const BUILD_VERSION = "adff83e102842dcc";
+const BUILD_VERSION = "d4f763f5a3cc743e";
 const currentCacheName = () => `${CACHE_PREFIX}${BUILD_VERSION}`;
 
 async function sha256(response) {
