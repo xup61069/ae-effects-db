65d83612ad32eb0b45f7969df94e5a73ec9f48af
xup61069 <xup61069@gmail.com>
2026-08-16 19:49:45 +0800

    feat: refresh aescripts New products

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 const MANIFEST_URL = "dist/web/asset-manifest.json";
 const CACHE_PREFIX = "ae-effects-db-";
-const BUILD_VERSION = "9c544ffe16737a20";
+const BUILD_VERSION = "2836caad24294eb7";
 const currentCacheName = () => `${CACHE_PREFIX}${BUILD_VERSION}`;
 
 async function sha256(response) {
