727a739fed7ff0d53e97379f9db382b852ace5bd
xup61069 <xup61069@gmail.com>
2026-08-17 02:17:53 +0800

    service-worker: sync BUILD_VERSION to current build hash adff83e102842dcc

--- ae-effects-db/service-worker.js
+++ ae-effects-db/service-worker.js
@@ -1,6 +1,6 @@
 const MANIFEST_URL = "dist/web/asset-manifest.json";
 const CACHE_PREFIX = "ae-effects-db-";
-const BUILD_VERSION = "eb13fc301f069150";
+const BUILD_VERSION = "adff83e102842dcc";
 const currentCacheName = () => `${CACHE_PREFIX}${BUILD_VERSION}`;
 
 async function sha256(response) {
