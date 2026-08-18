db6f6d14fb41b9d29789049d742ea7e69583ff17
xup61069 <xup61069@gmail.com>
2026-08-18 19:41:21 +0800

    chore: sync service-worker BUILD_VERSION to match dist manifest

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 const MANIFEST_URL = "dist/web/asset-manifest.json";
 const CACHE_PREFIX = "ae-effects-db-";
-const BUILD_VERSION = "2168c6593a41ba17";
+const BUILD_VERSION = "179f4263406bf009";
 const currentCacheName = () => `${CACHE_PREFIX}${BUILD_VERSION}`;
 
 async function sha256(response) {
